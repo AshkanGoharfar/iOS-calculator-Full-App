a162827d5ee9009f6d739db1bf3bdda2e3623d6f
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2021-10-21 14:43:40 +0330

    Initial Commit

---
+++ iOS-calculator-Full-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-calculator-Full-App
+//
+//  Created by Ashkan Goharfar on 7/29/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
