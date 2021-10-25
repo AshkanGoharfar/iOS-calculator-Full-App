e61f6f5bdf993103480bcf1e5b1f72ac4ec4cba4
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2021-10-26 02:09:08 +0330

    Landscape orientation has been fixed.

--- iOS-calculator-Full-App/ViewController.swift
+++ iOS-calculator-Full-App/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ResultDisplayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
